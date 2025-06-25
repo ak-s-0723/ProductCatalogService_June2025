ae5b135c0c59a9c086cbb9f8fd7fae1d72faf960
khannaanurag1 <khannaanurag1@gmail.com>
2025-06-25 22:17:50 +0530

    initial commit

---
+++ src/test/java/org/example/productcatalogservice_june2025/ProductCatalogServiceJune2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productcatalogservice_june2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogServiceJune2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
