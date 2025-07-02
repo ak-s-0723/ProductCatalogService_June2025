c9dece0c3af156a19cc2c1539844a7cd05d5f2fd
khannaanurag1 <khannaanurag1@gmail.com>
2025-07-02 23:00:50 +0530

    Integrated Mysql

--- src/main/java/org/example/productcatalogservice_june2025/models/Product.java
+++ src/main/java/org/example/productcatalogservice_june2025/models/Product.java
@@ -1,10 +1,14 @@
 package org.example.productcatalogservice_june2025.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Entity
 public class Product extends BaseModel {
     private String name;
 
@@ -14,6 +18,7 @@ public class Product extends BaseModel {
 
     private String imageUrl;
 
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
 
     //Related to Business
