e0a704389f8f061189b5093fec09dce4f49cb747
khannaanurag1 <khannaanurag1@gmail.com>
2025-07-16 23:21:21 +0530

    testing

--- src/main/java/org/example/productcatalogservice_june2025/models/Category.java
+++ src/main/java/org/example/productcatalogservice_june2025/models/Category.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -20,6 +21,7 @@ public class Category extends BaseModel {
 
     @OneToMany(mappedBy = "category",fetch= FetchType.LAZY)
     @Fetch(FetchMode.SELECT)
+    @BatchSize(size=2)
     private List<Product> products;
 }
 
--- src/test/java/org/example/productcatalogservice_june2025/repos/CategoryRepoTest.java
+++ src/test/java/org/example/productcatalogservice_june2025/repos/CategoryRepoTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,9 +22,20 @@ class CategoryRepoTest {
     @Transactional //Why I am adding this ?
     public  void testLoading() {
         Optional<Category> categoryOptional = categoryRepo.findById(2L);
-//        for(Product p : categoryOptional.get().getProducts()) {
-//            System.out.println(p.getName());
-//        }
+        for(Product p : categoryOptional.get().getProducts()) {
+            System.out.println(p.getName());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void testNPlus1() {
+        List<Category> categoryList = categoryRepo.findAll();
+        for(Category category : categoryList) {
+            for(Product product : category.getProducts()) {
+                System.out.println(product.getName());
+            }
+        }
     }
 
 }
